630cea8901cc15e02300f463fea4ddf290480a0f
Hitawasana17 <email-github-anda@gmail.com>
2026-09-15 16:40:44 +0700

    feat: add initial Playwright tests

---
+++ tests/Learn.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('learn pw on website formy project-autocomplete page', async ({ page }) => {
+  await page.goto('https://formy-project.herokuapp.com/')
+
+  // await page.locator('[href="/autocomplete"]').nth(1).click()
+
+  await page.getByRole('link', {name:'Autocomplete'}).click()
+  await page.waitForTimeout(3000)
+
+  //validasi
+  await expect(page).toHaveURL('https://formy-project.herokuapp.com/autocomplete');
+
+  //Locator dari Playwright UI
+  await page.getByPlaceholder('Enter address').fill('Semanggi')
+  await page.getByPlaceholder('Street address', {exact: true}).fill('Jl. Gatot Subroto')
+  await page.getByPlaceholder('Street address 2').fill('Apartemen Tamansari Semanggi')
+  await page.getByPlaceholder('City').fill('Jakarta')
+  await page.getByPlaceholder('State').fill('South Jakarta')
+  await page.getByPlaceholder('Zip code').fill('12930')
+  await page.getByPlaceholder('Country').fill('Indonesia')
+});
